6bbfb8b314d7a6a62a430ed045226a07a0d10f47
Arunav Bhattacharya <itsarunav4u@gmail.com>
2020-05-29 17:03:25 -0700

    feat(oop) : cheatsheets

--- 2-js-oop/2-prototypes-oop.js
+++ 2-js-oop/2-prototypes-oop.js
@@ -1,27 +1,34 @@
-let obj = {id : 1234};
+let obj = { id: 1234 };
 
 // Constructor function 
 function Circle(radius) {
-   this.radius = radius;
-   this.draw = function () { }
+    this.radius = radius;
+    this.draw = function () { }
 }
 
+function circle(){
+    return 'A new circle';
+}
+
+const aCircle = circle();
 // 1. Every object (except the root object) has a prototype (parent). 
 // To get the prototype of an object:
-Object.getPrototypeOf(obj); 
+console.log(Object.getPrototypeOf(obj));
 
 // In Chrome, you can inspect "__proto__" property. But you should 
 // not use that in the code. 
 
 // 2. To get the attributes of a property:
-Object.getOwnPropertyDescriptor(obj, 'propertyName');
+Object.getOwnPropertyDescriptor(obj, 'propertyName')
+console.log(Object.getOwnPropertyDescriptor(obj, 'id'));
 
 // 3. To set the attributes for a property:
-Object.defineProperty(obj, 'propertyName', {
+Object.defineProperty(obj, 'id', {
     configurable: false,    // cannot be deleted
     writable: false,
     enumerable: false
 });
+console.log(Object.getOwnPropertyDescriptor(obj, 'id'));
 
 // 4. Constructors have a "prototype" property. It returns the object 
 // that will be used as the prototype for objects created by the constructor. 
@@ -40,10 +47,10 @@ Object.getPrototypeOf(x) === Object.getPrototypeOf(y); // returns true
 // 6. When dealing with large number of objects, it's better to put their
 // methods on their prototype. This way, a single instance of the methods
 // will be in the memory. 
-Circle.prototype.draw = function() {}
+Circle.prototype.draw = function () { }
 
 // 7. To get the own/instance properties:
 Object.keys(obj);
 
 // To get all the properties (own + prototype): 
-for (let key in obj) {}
+for (let key in obj) { }
